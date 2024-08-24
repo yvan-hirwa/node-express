d85d5c4eb77ae6522f7fdb5649f133199fdc993c
hirwkar <hirwakaranganwayvan@gmail.com>
2024-08-24 21:00:37 +0200

    implemented basic routing

--- content/myfirstWebserver.js
+++ content/myfirstWebserver.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the Server');
+    const pathName = req.url
+    
+    if (pathName === '/product') res.end('Hello this is product page');
+    else if (pathName === '/about') res.end('Hello this is about page');
+    else if (pathName === '/') res.end('welcome to the homepage');
+    else { 
+            res.writeHead(404);
+            res.end('error: 404 page not found');
+        }
+    
 });
 
 server.listen(3400, 'localhost', () => {
